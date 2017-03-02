b6bc8f6dba15c2224ef92591528fd02941813c75
wujunyu <1318543125@qq.com>
2017-03-02 22:21:02 +0800

    excel的生成和导出

---
+++ src/com/imooc/drdc/utils/DB.java
@@ -0,0 +1,66 @@
+/**
+* 2015-7-26  
+* DB.java 
+* author:JunYu Wu 
+*/
+package com.imooc.drdc.utils;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB {
+	public static Connection createConn() {
+		Connection conn = null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=drdc", "sa", "123");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public static PreparedStatement prepare(Connection conn, String sql) {
+		PreparedStatement ps = null;
+		try {
+			ps = conn.prepareStatement(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ps;
+	}
+	
+	public static void close(Connection conn) {
+		
+		try {
+			conn.close();
+			conn = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void close(Statement stmt) {
+		try {
+			stmt.close();
+			stmt = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void close(ResultSet rs) {
+		try {
+			rs.close();
+			rs = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
